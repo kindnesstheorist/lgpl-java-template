a062c84c9ae355178978e4387ff07a483a2e1b90
Kenneth Fraser <kenneth-fraser@outlook.com>
2019-02-14 01:27:16 -0800

    A Java Hello World program created using Eclipse.
    Released under the LGPL.

---
+++ githubtest/src/com/dreamwidth/kindnesstheorist/javatest/JavaTestMain.java
@@ -0,0 +1,36 @@
+/*
+ * Copyright Kenneth Fraser 2019
+ * 
+ * This program is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation, either version 3 of the License, or (at your option) any
+ * later version.
+ * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package com.dreamwidth.kindnesstheorist.javatest;
+
+/**
+ * A class created for the purpose of testing. Currently quite useless.
+ */
+public class JavaTestMain
+{
+
+	/**
+	 * Prints "hello world" followed by the identifier "gouijfo".
+	 * 
+	 * @param args used to pad out method signature; value is discarded
+	 */
+	public static void main(String[] args)
+	{
+		System.out.println("hello world gouijfo");
+	}
+
+}
